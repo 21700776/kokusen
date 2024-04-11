570b4063952eaa1bf54bbcc09ee36b740cfa4d9a
21700776 <21700776@handong.ac.kr>
2024-04-11 13:34:57 +0900

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,14 @@
+import { Inter, Nanum_Myeongjo, Noto_Sans_KR } from 'next/font/google';
+
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+export const NotoSansKR = Noto_Sans_KR({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+  });
+
+  export const NanumMyeongjo = Nanum_Myeongjo({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+  });
